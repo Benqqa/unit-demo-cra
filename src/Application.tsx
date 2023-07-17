be2ff321467fa0de9dce4334b0a56a4714b0a663
Maxim Benyuh <max.bobo3@bk.ru>
2023-07-17 16:23:46 +0300

    fix: umer ya

--- src/Application.tsx
+++ src/Application.tsx
@@ -4,7 +4,6 @@ import { Link, Route, Routes } from "react-router-dom";
 import { About } from "./pages/About";
 import { Home } from "./pages/Home";
 
-
 export const Application: FC = () => {
   const home = <Home />;
   const about = <About />;
