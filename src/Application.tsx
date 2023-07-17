293ed560d3d9616eba9e98b5f2c83af8002d54c8
Maxim Benyuh <max.bobo3@bk.ru>
2023-07-17 13:37:21 +0300

    fix: test 22.1

--- src/Application.tsx
+++ src/Application.tsx
@@ -5,6 +5,7 @@ import { Link, Route, Routes } from "react-router-dom";
 import { About } from "./pages/About";
 import { Home } from "./pages/Home";
 
+
 export const Application: FC = () => {
   const home = <Home />;
   const about = <About />;
